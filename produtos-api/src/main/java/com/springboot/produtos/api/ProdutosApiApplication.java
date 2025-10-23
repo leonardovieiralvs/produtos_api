b929ae54c012f02ea7d53d884330c6e07ddc2d03
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-10-23 05:27:21 -0300

    produtos_api

---
+++ produtos-api/src/main/java/com/springboot/produtos/api/ProdutosApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.produtos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosApiApplication.class, args);
+	}
+
+}
