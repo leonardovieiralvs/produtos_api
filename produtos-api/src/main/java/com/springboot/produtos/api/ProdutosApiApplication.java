e2f47aa7f5e3c5ee369ce9427823fbd5be7605b4
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-10-25 00:08:33 -0300

    mapeamento jpa

--- produtos-api/src/main/java/com/springboot/produtos/api/ProdutosApiApplication.java
+++ produtos-api/src/main/java/com/springboot/produtos/api/ProdutosApiApplication.java
@@ -2,18 +2,9 @@ package com.springboot.produtos.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ProdutosApiApplication {
-
-	@GetMapping("/hello")
-	public String helloWorld() {
-		return "Hello World!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosApiApplication.class, args);
