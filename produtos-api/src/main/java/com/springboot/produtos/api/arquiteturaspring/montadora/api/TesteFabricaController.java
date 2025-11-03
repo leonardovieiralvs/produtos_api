915fbfdcfb7e575a4aa8e515a12bd9b6eabbc385
Leonardo <leonardo_vieira199@hotmail.com>
2025-11-03 20:20:14 -0300

    utilizando Qualifer + Bean

--- produtos-api/src/main/java/com/springboot/produtos/api/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ produtos-api/src/main/java/com/springboot/produtos/api/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -3,6 +3,7 @@ package com.springboot.produtos.api.arquiteturaspring.montadora.api;
 import com.springboot.produtos.api.arquiteturaspring.montadora.*;
 import com.springboot.produtos.api.arquiteturaspring.montadora.configuration.MontadoraConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TesteFabricaController {
 
     @Autowired
+    @Qualifier("motorEletrico")
     private Motor motor;
 
     @PostMapping
--- produtos-api/src/main/java/com/springboot/produtos/api/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
+++ produtos-api/src/main/java/com/springboot/produtos/api/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
@@ -9,13 +9,35 @@ import org.springframework.context.annotation.Configuration;
 public class MontadoraConfiguration {
 
     @Bean
-    public Motor motor() {
+    public Motor motorAspirado() {
         Motor motor = new Motor();
         motor.setCavalo(120);
         motor.setCilindros(4);
         motor.setModelo("XPTO-0");
         motor.setLitragem(2.0);
         motor.setTipoMotor(TipoMotor.ASPIRADO);
+        return motor;
+    }
+
+    @Bean
+    public Motor motorEletrico() {
+        Motor motor = new Motor();
+        motor.setCavalo(100);
+        motor.setCilindros(4);
+        motor.setModelo("XPTO-1");
+        motor.setLitragem(2.0);
+        motor.setTipoMotor(TipoMotor.ELETRICO);
+        return motor;
+    }
+
+    @Bean
+    public Motor motorTurbo() {
+        Motor motor = new Motor();
+        motor.setCavalo(110);
+        motor.setCilindros(4);
+        motor.setModelo("XPTO-2");
+        motor.setLitragem(2.0);
+        motor.setTipoMotor(TipoMotor.TURBO);
         return motor;
     }
 }
